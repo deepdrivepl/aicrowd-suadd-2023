c48ef1627d5878e6684a68e96336cd003ed7eaac
amrukwa <a.mrukwa00@gmail.com>
2023-01-19 20:58:02 +0100

    fix names

--- semantic_segmentation/scripts/train.py
+++ semantic_segmentation/scripts/train.py
@@ -20,10 +20,10 @@ from augment import get_transform
 from dataset import Dataset
 
 
-class Suad_Semseg(pl.LightningModule):
-    def __init__(self, arch, lr, num_classes, **kwargs):
+class SuadSemseg(pl.LightningModule):
+    def __init__(self, net, lr, num_classes, **kwargs):
         super().__init__()
-        self.net = arch()
+        self.net = net
         self.loss = nn.CrossEntropyLoss(ignore_index=255)
         self.lr = lr
         self.accuracy = Accuracy(
@@ -119,12 +119,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--datafolder", type=str, default="")
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--arch", type=str, default="u2net")
-    parser = Suad_Semseg.add_model_specific_args(parser)
+    parser.add_argument("--net", type=str, default="u2net")
+    parser = SuadSemseg.add_model_specific_args(parser)
     args = parser.parse_args()
     dict_args = vars(args)
 
-    arch = architectures[args.arch]
+    net = architectures[args.net]
 
     train_ds = Dataset(args.datafolder, get_transform(train=True))
     val_ds = Dataset(args.datafolder, get_transform(), train=False)
@@ -135,8 +135,8 @@ if __name__ == "__main__":
         val_ds, batch_size=args.batch_size, shuffle=True, num_workers=8
     )
 
-    model = Suad_Semseg(arch, **dict_args)
+    model = SuadSemseg(net, **dict_args)
     logger = TensorBoardLogger("tb_logs", name="u2net")
     callback = EarlyStopping(monitor="val_loss", mode="min", patience=5)
-    trainer = pl.Trainer(max_epochs=100, logger=logger, callbacks=[callback])
+    trainer = pl.Trainer(max_epochs=100, logger=logger, callbacks=[callback], accelerator='gpu')
     trainer.fit(model, train_loader, val_loader)
